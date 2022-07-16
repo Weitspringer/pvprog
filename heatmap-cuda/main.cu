f6c6b59f9e0623ee77d275ccd591940dfeff14c1
Weitspringer <luca_springer@yahoo.com>
2022-07-16 13:47:53 +0200

    ⚡️ CUDA with less copy overhead

--- heatmap-cuda/main.cu
+++ heatmap-cuda/main.cu
@@ -18,6 +18,10 @@ __global__ void swapDataWithFutureData(Heatmap* d_heatmap)
     d_heatmap->overrideDataWithFutureData();
 }
 
+__global__ void printDeviceData(Heatmap* heatmap) {
+    heatmap->printFormattedOutputDevice();
+}
+
 int main(int argc, char **argv)
 {
     if (argc < 3)
@@ -70,6 +74,8 @@ int main(int argc, char **argv)
     // Make an allocated region on device for use by pointer in class
     double *d_data;
     cudaMalloc((void **)&d_data, sizeof(double)*numberOfElements);
+    cudaMemcpy(d_data, heatmap.data, sizeof(double)*numberOfElements, cudaMemcpyHostToDevice);
+    cudaMemcpy(&(d_heatmap->data), &d_data, sizeof(double *), cudaMemcpyHostToDevice);
     double *d_futureData;
     cudaMalloc((void **)&d_futureData, sizeof(double)*numberOfElements);
     cudaMemcpy(&(d_heatmap->futureData), &d_futureData, sizeof(double *), cudaMemcpyHostToDevice);
@@ -77,17 +83,34 @@ int main(int argc, char **argv)
     for (int i = 0; i < numberOfRounds; i++) 
     {
         // Update of hotspots in round i
-        updateHotspots(heatmap, lifecycles, i);
-        cudaMemcpy(d_data, heatmap.data, sizeof(double)*numberOfElements, cudaMemcpyHostToDevice);
-        cudaMemcpy(&(d_heatmap->data), &d_data, sizeof(double *), cudaMemcpyHostToDevice);
+        // Get active cells for round i
+        vector<pair<int, int>> activeCellsVector = lifecycles.getCellsByRound(i);
+        int numberOfActiveCells = activeCellsVector.size();
+        pair<int, int>* activeCellsArray = &activeCellsVector[0];
+        // Copy to device
+        pair<int, int>* d_activeCells;
+        cudaMalloc((void **)&d_activeCells, sizeof(pair<int, int>)*numberOfActiveCells);
+        cudaMemcpy(d_activeCells, activeCellsArray, sizeof(pair<int, int>)*numberOfActiveCells, cudaMemcpyHostToDevice);
+        updateHotspotsDevice<<<1, 1>>>(d_heatmap, d_activeCells, numberOfActiveCells);
+        cudaFree(d_activeCells);
         // Run simulation kernel
         simulateRoundWithCuda<<<threadsPerBlock, blocksPerGrid>>>(d_heatmap, numberOfElements);
         swapDataWithFutureData<<<1,1>>>(d_heatmap);
-        // Copy data to host
-        cudaMemcpy(heatmap.data, d_futureData, sizeof(double)*numberOfElements, cudaMemcpyDeviceToHost);
         // Update of hotspots in round i+1
-        updateHotspots(heatmap, lifecycles, i);
+        // Get active cells for round i+1
+        activeCellsVector = lifecycles.getCellsByRound(i+1);
+        numberOfActiveCells = activeCellsVector.size();
+        activeCellsArray = &activeCellsVector[0];
+        cudaMalloc((void **)&d_activeCells, sizeof(pair<int, int>)*numberOfActiveCells);
+        cudaMemcpy(d_activeCells, activeCellsArray, sizeof(pair<int, int>)*numberOfActiveCells, cudaMemcpyHostToDevice);
+        updateHotspotsDevice<<<1, 1>>>(d_heatmap, d_activeCells, numberOfActiveCells);
+        cudaFree(d_activeCells);
     }
+    
+    // Copy data to host
+    //printDeviceData<<<1,1>>>(d_heatmap);
+    cudaMemcpy(&d_data, &(d_heatmap->data), sizeof(double *), cudaMemcpyDeviceToHost);
+    cudaMemcpy(heatmap.data, d_data, sizeof(double)*numberOfElements, cudaMemcpyDeviceToHost);
     
     // Cleanup
     cudaFree(d_heatmap);
